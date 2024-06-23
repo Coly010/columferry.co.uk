2d96f277d4de85d0e6926532d8159d5b5e54b608
Colum Ferry <cferry09@gmail.com>
2024-06-23 21:49:09 +0100

    fix: log analytics correctly

--- app/components/use-firebase.tsx
+++ app/components/use-firebase.tsx
@@ -2,31 +2,35 @@ import { type FirebaseApp, initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { useHydrated } from "./use-hydrated";
 
+let loggingAnalytics: number | undefined;
 let isInitialRequest = true;
 let firebaseApp: FirebaseApp;
-
-export function clientLoader() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBlyVoECJ_I312_g_ye8yUA6D1vVqWNiy8",
-    authDomain: "columferry-co-uk-c6fd6.firebaseapp.com",
-    projectId: "columferry-co-uk-c6fd6",
-    storageBucket: "columferry-co-uk-c6fd6.appspot.com",
-    messagingSenderId: "424257855983",
-    appId: "1:424257855983:web:2f8ce62ad99373f71cd61a",
-    measurementId: "G-0KVMSX9TP4",
-  };
-
-  if (isInitialRequest) {
-    isInitialRequest = false;
-    firebaseApp = initializeApp(firebaseConfig);
-  }
-  const analytics = getAnalytics(firebaseApp);
-  logEvent(analytics, "page_view", {});
-
-  return null;
-}
-clientLoader.hydrate = true;
+const firebaseConfig = {
+  apiKey: "AIzaSyBlyVoECJ_I312_g_ye8yUA6D1vVqWNiy8",
+  authDomain: "columferry-co-uk-c6fd6.firebaseapp.com",
+  projectId: "columferry-co-uk-c6fd6",
+  storageBucket: "columferry-co-uk-c6fd6.appspot.com",
+  messagingSenderId: "424257855983",
+  appId: "1:424257855983:web:2f8ce62ad99373f71cd61a",
+  measurementId: "G-0KVMSX9TP4",
+};
 
 export function FirebaseAnalytics() {
+  if (typeof window !== "undefined") {
+    if (document.readyState === "complete") {
+      if (isInitialRequest) {
+        isInitialRequest = false;
+        firebaseApp = initializeApp(firebaseConfig);
+      }
+      if (!loggingAnalytics) {
+        loggingAnalytics = window.setTimeout(() => {
+          const analytics = getAnalytics(firebaseApp);
+          logEvent(analytics, "page_view", {});
+          window.clearTimeout(loggingAnalytics);
+          loggingAnalytics = undefined;
+        }, 250);
+      }
+    }
+  }
   return useHydrated() ? <>&nbsp;</> : <>&nbsp;</>;
 }
